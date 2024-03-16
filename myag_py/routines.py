baa911c84c69b47b68c754a77e7c3191552b183d
Aubery Lis <analogpatch@gmail.com>
2024-03-16 13:13:49 +0200

    thumb routines now working

--- myag_py/routines.py
+++ myag_py/routines.py
@@ -1,3 +1,7 @@
+### !!!!!
+SETTING_thumbSize = 300 #px
+
+
 import json
 import xml.etree.ElementTree as xmlet
 import xml.dom as xmldom
@@ -39,18 +43,27 @@ def routine_thumbs_xml(channel_instance, post_request, neocities):
 	post_request.form['datafiles'] = json.dumps(json_datafiles)
 
 
+# delete thumbs of the deleted files
+def routine_thumbs_delete(channel_instance, post_request, neocities):
+	# since we already deleted the actual entries from local xml we're gonna have to fetch the previos version over web
+	# yes, this is not pretty, but this is the price of handling thumbnails entirely in here instead of
+	# adding this functionality to the package itself, which should be able to run without the neomanager backend
+
+	remote_folder = os.path.join(channel_instance.location, "myag_files").replace('./', '')
+	fd = urlopen(os.path.join("https://{}.neocities.org".format(nm.fetchSiteName(neocities.api_key)), remote_folder, "data.xml"))
+	xml_io = io.BytesIO(fd.read())
+	xml_wrapper = io.TextIOWrapper(xml_io, encoding='utf-8')
+	xml = xmlet.fromstring(xml_wrapper.read())
+
+	for dname in json.loads(post_request.form['deletenames']):
+		thumb_name = thumb_by_filename(xml, os.path.basename(dname))
+		not thumb_name:
+			continue
+		thumb_fullname = os.path.join(channel_instance.location, "myag_artworks", thumb_name)		
+		neocities.delete(thumb_fullname)
 
 
-def filename_by_awid(xmldoc, awid):
-	for aw in xmldoc.find("artworks").findall("artwork"):
-		if aw.findtext("awid") == awid:
-			return aw.findtext("filename")
-	return False
-
-
-
-
-def routine_thumbs_files(channel_instance, post_request, neocities):
+def routine_thumbs_create(channel_instance, post_request, neocities):
 	
 	remote_folder = os.path.join(channel_instance.location, "myag_artworks").replace('./', '')
 	remote_names = json.loads(post_request.form['uploadnames'])
@@ -58,7 +71,7 @@ def routine_thumbs_files(channel_instance, post_request, neocities):
 	# this checks AWIDs that were fixed in routine_thumbs_xml, and if any of them are not a fresh upload - make a thumb from remote image
 	xml = xmldoc(post_request)
 	for awid in awids_without_thumbnails:
-		if [upload for upload in remote_names if awid_from_filename(upload["remote_name"]) == awid] == []:
+		if [upload for upload in remote_names if filename_noext(upload["remote_name"]) == awid] == []:
 			# we did not find this awid in new uploads
 			# this means it's an old artwork that has no thumb; grabbing image remotely
 			original = filename_by_awid(xml, awid)
@@ -74,14 +87,7 @@ def routine_thumbs_files(channel_instance, post_request, neocities):
 		awid = os.path.splitext(os.path.basename(next(upload for upload in remote_names if upload["upload_tag_id"] == upload_id)["remote_name"]))[0] #AHAHAHAHAH ! :D
 		send_thumb(remote_folder, local_name, awid, neocities)
 
-
-def awid_from_filename(arg):
-	return os.path.splitext(os.path.basename(arg))[0]
-
-def make_thumb_name(arg):
-	return "{}_thumb.png".format(arg)
-
-
+	
 def send_thumb(remote_folder, file, awid, neocities, local=True):
 	image = None
 	if local:
@@ -103,9 +109,7 @@ def send_thumb(remote_folder, file, awid, neocities, local=True):
 	neocities.upload(tup)
 
 
-
-
-def make_thumb(pil_image, thumb_bigger_side = 200):
+def make_thumb(pil_image, thumb_bigger_side = SETTING_thumbSize):
 	w, h = pil_image.size
 	bigger_side = max(w, h)
 	if bigger_side < thumb_bigger_side:
@@ -113,3 +117,25 @@ def make_thumb(pil_image, thumb_bigger_side = 200):
 	ratio =  thumb_bigger_side/bigger_side
 	newsize = (floor(w*ratio), floor(h*ratio))
 	return pil_image.resize(newsize)
+
+
+def filename_noext(arg):
+	return os.path.splitext(os.path.basename(arg))[0]
+
+def make_thumb_name(arg):
+	return "{}_thumb.png".format(arg)
+
+def filename_by_awid(xmldoc, awid):
+	for aw in xmldoc.find("artworks").findall("artwork"):
+		if aw.findtext("awid") == awid:
+			return aw.findtext("filename")
+	return False
+
+
+def thumb_by_filename(xmldoc, fname):
+	for aw in xmldoc.find("artworks").findall("artwork"):
+		print("\n\n\n{} - {}\n\n\n".format(aw.findtext("filename"), fname))
+		if aw.findtext("filename") == fname:
+			return aw.findtext("thumbnail")
+	return False
+
